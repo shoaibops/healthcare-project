688fe9a518b33f9a4fe7fd0875867a8229aedfbc
shoaibops <137149864+shoaibops@users.noreply.github.com>
2025-03-30 23:15:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,9 +7,9 @@ data "aws_security_group" "existing_sg" {
 
 resource "aws_instance" "ec2-prod" {
   count = 2 # Will create 2 machines,
-  ami           = "ami-04b4f1a9cf54c11d0"
+  ami           = "ami-0e35ddab05955cf57"
   instance_type = "t3.medium"
-  key_name      = "jenkins-master"   # Name of the existing key pair in AWS
+  key_name      = "aws-key"   # Name of the existing key pair in AWS
 
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
